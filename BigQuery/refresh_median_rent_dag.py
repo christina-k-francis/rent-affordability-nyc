46b4985d69eb37b64a4054d518ee4e0194e138d9
Christina Francis <CFranc22@jhu.edu>
2025-10-13 14:50:07 -0400

    Created DAG airflow orchestration files for refreshing analysis tables following main table updates

---
+++ BigQuery/refresh_median_rent_dag.py
@@ -0,0 +1,62 @@
+"""
+DAG Orchestration script that checks for BigQuery staging_median_rent table changes,
+and then runs the corresponding SQL file, thus refreshing all related analysis tables
+"""
+
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCheckOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from datetime import datetime, timedelta
+
+# Configuration
+PROJECT_ID = "rent-affordability"
+DATASET_ID = "nyc_analysis"
+STAGING_TABLE = "staging_median_rent"
+SQL_PATH = "/home/airflow/gcs/dags/sql/refresh_median_rent.sql"
+
+default_args = {
+    "owner": "Christina",
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="refresh_median_rent_dag",
+    default_args=default_args,
+    description="Run refresh SQL when staging_median_rent table changes",
+    schedule_interval="*/15 * * * *",  # checks every 15 minutes
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["bigquery", "refresh", "dag", "rent"],
+) as dag:
+
+    # Sensor: Check if the staging table has changed since last run
+    wait_for_rent_table = BigQueryCheckOperator(
+        task_id="wait_for_staging_income_update",
+        sql=f"""
+            SELECT TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), TIMESTAMP_MILLIS(last_modified_time), MINUTE)
+            FROM `{PROJECT_ID}.{DATASET_ID}.__TABLES__`
+            WHERE table_id = '{STAGING_TABLE}'
+            AND TIMESTAMP_MILLIS(last_modified_time) > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 HOUR)
+        """,
+        use_legacy_sql=False,
+        do_xcom_push=False,
+        retries=0,
+        retry_delay=timedelta(minutes=15),
+        fail_on_empty=False  # this allows DAG to continue even if no updated rows are found
+    )
+
+    # Task: Run the refresh SQL
+    refresh_rent = BigQueryInsertJobOperator(
+        task_id="run_refresh_rent_sql",
+        configuration={
+            "query": {
+                "query": open(SQL_PATH).read(),
+                "useLegacySql": False,
+            }
+        },
+        location="US",
+    )
+
+    wait_for_rent_table >> refresh_rent
